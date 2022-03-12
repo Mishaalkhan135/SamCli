8d097477fbcb73300d9978e1f07d3ae537e21efc
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-03-12 22:45:44 +0500

    done

--- cdk-sam-example/lib/cdk-sam-example-stack.ts
+++ cdk-sam-example/lib/cdk-sam-example-stack.ts
@@ -1,32 +1,19 @@
-import { Stack, StackProps ,CfnOutput} from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway'
-// import * as sqs from 'aws-lib-cdk/aws-sqs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkSamExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const hello = new lambda.Function(this, "hello-mishaal-handler", {
-      runtime: lambda.Runtime.NODEJS_12_X, // Specifying which type of function we have deployed
-      code: lambda.Code.fromAsset("lambda"), // 'lambda' is the folder containing all lambda function
-      handler: "inedx.handler", // 'hello' is the file name and 'handler' is the function name
-      functionName:"lambda-example"
+    // The code that defines your stack goes here
+    new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'app.lambda_handler',
+      code:    lambda.Code.fromAsset('./my_function'),
+      functionName:'samCli'
     });
-    const api = new apigw.LambdaRestApi(this, 'my-api-1', {
-      restApiName: "my-first-api",
-      handler: hello,
-      proxy: false
-    });
-     const items = api.root.addResource('cars');
-     const items_2 = api.root.addResource('bikes');
-    items.addMethod('GET');// GET cars
-    items_2.addMethod('GET');  
-
-    new CfnOutput(this, 'api-url-output', {
-      value: api.url
-    })
 
 
   }
